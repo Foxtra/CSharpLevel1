be2bd54257d71724b3ad2c2739a775932ebead22
Foxtra <tkachew.sergey@ya.ru>
2019-02-02 16:47:55 +0300

    Commit branch with homework to Lesson 2

---
+++ Lesson2/AmountOfOddNumbers/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+ * Ткачёв Сергей
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmountOfOddNumbers
+{
+    class Program
+    {
+        static int GetInt() //проверяем ввод
+        {
+            while (true)
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                    Console.Write("Неверный ввод (требуется числовое значение).\nПожалуйста повторите ввод: ");
+                else return x;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа подсчета нечетных положительных чисел");
+            Console.WriteLine("Вводите числа по одному, для завершения введите 0: ");
+
+            int AmountOfOddNumbers = 0;
+            while (true) 
+            {
+                int number = GetInt();
+                if (number == 0)
+                {
+                    break;
+                }
+                else if(number > 0 && number % 2 == 1 )
+                {
+                    AmountOfOddNumbers++;
+                }
+            } 
+
+            Console.WriteLine(Environment.NewLine + "Количество чисел: " + AmountOfOddNumbers);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson2/Authorization/Program.cs
@@ -0,0 +1,103 @@
+﻿/*
+ *  Реализовать метод проверки логина и пароля. На вход подается логин и пароль. 
+ *  На выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains). 
+ *  Используя метод проверки логина и пароля, написать программу: пользователь вводит логин и пароль, 
+ *  программа пропускает его дальше или не пропускает. С помощью цикла do while ограничить ввод пароля тремя попытками.
+ *  
+ *  Ткачёв Сергей
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization
+{
+    class Program
+    {
+
+        static string enterPass()
+        {
+            string pass = "";
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                // Backspace и Enter не учитываются
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    pass += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
+                    {
+                        pass = pass.Substring(0, (pass.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                }
+            } while (true);
+
+            return pass;
+        }
+
+        static string RightTryWord(int x)
+        {
+            string s = "";
+            // Попытка, когда заканчивается на один, кроме 11.
+            if (x % 10 == 1 && x != 11) s += " попытка";
+            else
+                // Попытки
+                if ((x >= 2 && x <= 4) || (x >= 22 && x <= 24) || (x >= 32 && x <= 34) || (x > 41 && x < 45)) s += " попытки";
+            else
+                    // Попыток
+                    if ((x == 11) || (x >= 5 && x <= 20) || (x >= 25 && x <= 30) || (x >= 35 && x < 41) || (x > 44 && x < 51)) s += " попыток";
+            return s;
+        }
+
+        static bool CheckLogAndPass(string login, string password)
+        {
+            if (login == "root" && password == "GeekBrains")
+                return true;
+            else
+                return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа проверки логина и пароля.");
+            int AmountOfTries = 3;
+
+            do
+            {
+                Console.Write("Введите логин: ");
+                string login = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                string password = enterPass();
+
+                if (CheckLogAndPass(login, password))
+                {
+                    Console.WriteLine();
+                    break;
+                }
+                else
+                {
+                    AmountOfTries--;
+                    Console.WriteLine("Неверный ввод логина или пароля." + 
+                        Environment.NewLine + "У Вас осталось " + AmountOfTries + RightTryWord(AmountOfTries));
+                }
+
+            } while (AmountOfTries > 0);
+
+            Console.WriteLine("Авторизация успешна!");
+
+            Console.ReadKey();
+        }
+    }
+}
