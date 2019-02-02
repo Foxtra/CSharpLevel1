b79406c758958061acba277b02bc99d91ffb00d7
Foxtra <tkachew.sergey@ya.ru>
2019-02-02 16:49:03 +0300

    Commit branch with homework to Lesson 3

---
+++ Lesson3/Complex/CComplex.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * 1. а) Дописать структуру Complex, добавив метод вычитания комплексных чисел. Продемонстрировать работу структуры;
+ * б) Дописать класс Complex, добавив методы вычитания и произведения чисел. Проверить работу класса;
+ * Сергей Ткачёв
+ */
+
+namespace Complex
+{
+    /// <summary>
+    /// Класс для представления комплексного числа
+    /// </summary>
+    internal class CComplex
+    {
+        private double im;
+        double re;
+
+        /// <summary> Конструктор без параметров</summary>
+        public CComplex()
+        {
+            im = 0;
+            re = 0;
+        }
+        /// <summary> Конструктор с параметрами</summary>
+        /// <param name="_im">Мнимая часть</param>
+        /// <param name="re">Вещественная часть</param>
+        /// <returns></returns>
+        public CComplex(double _im, double re)
+        {
+            im = _im;
+            this.re = re;
+        }
+
+        /// <summary> Метод сложения с другим комплексным числом </summary>
+        /// <param name="x2">Комплексное число для сложения</param>
+        /// <returns></returns>
+        public CComplex Plus(CComplex x2)
+        {
+            CComplex x3 = new CComplex();
+            x3.im = x2.im + im;
+            x3.re = x2.re + re;
+            return x3;
+        }
+        /// <summary> Метод разности с другим комплексным числом </summary>
+        /// <param name="x2">Комплексное число для разности</param>
+        /// <returns></returns>
+        public CComplex Minus(CComplex x2)
+        {
+            CComplex x3 = new CComplex();
+            x3.im = im - x2.im;
+            x3.re = re - x2.re;
+            return x3;
+        }
+        /// <summary> Метод произведения с другим комплексным числом </summary>
+        /// <param name="x2">Комплексное число для произведения</param>
+        /// <returns></returns>
+        public CComplex Multi(CComplex x2)
+        {
+            CComplex x3 = new CComplex();
+            x3.im = re * x2.im + im * x2.re;
+            x3.re = re * x2.re - im * x2.im;
+            return x3;
+        }
+
+        /// <summary>Свойство для мнимой части</summary>
+        public double Im
+        {
+            get { return im; }
+            set
+            {
+                if (value >= 0) im = value;
+            }
+        }
+
+        /// <summary> Метод представления комплексного числа в удобной форме</summary>
+        public string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+
+    }
+}
---
+++ Lesson3/Fraction/Program.cs
@@ -0,0 +1,89 @@
+﻿/*
+ *Описать класс дробей - рациональных чисел, являющихся отношением двух целых чисел. 
+ * Предусмотреть методы сложения, вычитания, умножения и деления дробей. 
+ * Написать программу, демонстрирующую все разработанные элементы класса. 
+ ** Добавить проверку, чтобы знаменатель не равнялся 0. Выбрасывать исключение ArgumentException("Знаменатель не может быть равен 0");
+ ** Добавить упрощение дробей.
+ * Сергей Ткачёв
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    class Program
+    {
+        /// <summary>
+        /// Функция проверки ввода без обработки исключений
+        /// </summary>
+        /// <returns></returns>
+        static int GetInt()
+        {
+            while (true)
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                    Console.Write("Неверный ввод (требуется числовое значение).\nПожалуйста, повторите ввод: ");
+                else return x;
+        }
+
+        static Fraction Init(int numerator, int denumerator)
+        {
+            bool checkException;
+            Fraction fraction = null;
+            do
+            {
+                checkException = false;
+                try
+                {
+                    fraction = new Fraction(numerator, denumerator);
+                }
+                catch (ArgumentException ex)
+                {
+                    checkException = true;
+                    Console.WriteLine("Ошибка: " + ex.Message);
+                    Console.Write("Повторно введите знаменатель дроби: ");
+                    denumerator = GetInt();
+                }
+            } while (checkException);
+            
+            return fraction;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа работы с дробями!");
+            Console.Write("Введите числитель первой дроби: ");
+            int n1 = GetInt();
+            Console.Write("Введите знаминатель первой дроби: ");
+            int d1 = GetInt();
+            Console.Write("Введите числитель второй дроби: ");
+            int n2 = GetInt();
+            Console.Write("Введите знаминатель второй дроби: ");
+            int d2 = GetInt();
+            Fraction fraction1 = Init(n1, d1);
+            Fraction fraction2= Init(n2, d2);
+
+            Console.WriteLine();
+            Fraction result = fraction1.Plus(fraction2);
+            result.Simplification();
+            Console.WriteLine("Результом операции сложения чисел: " + fraction1.ToString() + " и " + fraction2.ToString() + " является "
+                + result.ToString());
+            result = fraction1.Minus(fraction2);
+            result.Simplification();
+            Console.WriteLine("Результом операции вычитания чисел: " + fraction1.ToString() + " и " + fraction2.ToString() + " является "
+               + result.ToString());
+            result = fraction1.Multi(fraction2);
+            result.Simplification();
+            Console.WriteLine("Результом операции умножения чисел: " + fraction1.ToString() + " и " + fraction2.ToString() + " является "
+               + result.ToString());
+            result = fraction1.Division(fraction2);
+            result.Simplification();
+            Console.WriteLine("Результом операции деления чисел: " + fraction1.ToString() + " и " + fraction2.ToString() + " является "
+               + result.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
