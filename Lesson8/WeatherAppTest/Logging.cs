fbf117014fd226a6e6704012b3059b893e1a961a
Foxtra <tkachew.sergey@ya.ru>
2019-02-17 18:45:05 +0300

    Коммит задания с погодой

---
+++ Lesson8/WeatherAppTest/Logging.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppTest
+{
+    /// <summary>Класс логирования действий пользователя</summary>
+    class Logging
+    {
+        /// <summary>Метод записи в файл</summary>
+        /// <param name="Msg">Текст сообщения</param>
+        internal static void Log(string Msg)
+        {
+            using (var sw = new System.IO.StreamWriter("data.log",true))
+            {
+                sw.WriteLine(Msg);
+            }
+        }
+    }
+}
---
+++ Lesson8/WeatherAppTest/Repository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace WeatherAppTest
+{
+    /// <summary>Класс для хранения набора данных о городах</summary>
+    class Repository
+    {
+        List<City> db = new List<City>();
+
+        public List<City> Citys { get { return this.db; } }
+
+        Random random = new Random();
+
+        /// <summary>Конструктор добавляет несколько городов</summary>
+        public Repository()
+        {
+            db.Add(GetWeather("Saint Petersburg", "69.xml"));
+            db.Add(GetWeather("Adler", "2879.xml"));
+            db.Add(GetWeather("Borovichi", "2799.xml"));
+            db.Add(GetWeather("Ufa", "140.xml"));
+            db.Add(GetWeather("Penza", "169.xml"));
+            db.Add(GetWeather("Kazan", "486.xml"));
+
+        }
+
+        /// <summary>Метод вывода всех городов в консоль</summary>
+        public void PrintData()
+        {
+            foreach (var item in db)
+            {
+                Debug.WriteLine(item.Print());
+            }
+        }
+
+        /// <summary>Метод получения данных о погоде конкретного города</summary>
+        /// <param name="city">Название города</param>
+        /// <param name="code">Код файла на веб-сервисе meteoservice.ru</param>
+        /// <returns></returns>
+        private static City GetWeather(string city, string code)
+        {
+            string url = "https://xml.meteoservice.ru/export/gismeteo/point/"+code;
+            WebClient webClient = new WebClient();
+            string data = webClient.DownloadString(url);
+
+            City currentCity = new City();
+
+            var weatherCollection = XDocument.Parse(data)
+                .Descendants("MMWEATHER")
+                .Descendants("REPORT")
+                .Descendants("TOWN")
+                .Descendants("FORECAST").ToArray();
+
+
+            currentCity.CityName = city;
+            currentCity.PressureMax = int.Parse(weatherCollection[0].Element("PRESSURE").Attribute("max").Value);
+            currentCity.PressureMin = int.Parse(weatherCollection[0].Element("PRESSURE").Attribute("min").Value);
+            currentCity.TemperatureMax = int.Parse(weatherCollection[0].Element("TEMPERATURE").Attribute("max").Value);
+            currentCity.TemperatureMin = int.Parse(weatherCollection[0].Element("TEMPERATURE").Attribute("min").Value);
+
+
+            return currentCity;
+        }
+
+    }
+}
